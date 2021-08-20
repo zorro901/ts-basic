67e5673d09cbec017c850431add94d7474a677e4
zorro901 <zoro907@gmail.com>
2021-08-20 21:14:17 +0900

    関数のパラメーターと戻り値に型をつける

---
+++ src/function/basic.ts
@@ -0,0 +1,34 @@
+// アロー関数
+export const logMessage = (message:string):void => {
+    console.log("function basic sample 1:",message) 
+} 
+// 名前付き関数
+export function logMessage2(message:string):void {
+    console.log("function basic sample 2:",message) 
+} 
+
+// 関数式
+export const logMessage3 = function(message:string):void{
+    console.log("function basic sample 3:",message) 
+}
+
+// アロー関数の省略記法
+export const logMessage4 = (message:string):void => console.log("function basic sample 4:",message) 
+
+export const alwaysThrowError = (message:string):never => {
+    throw new Error(message)
+}
+
+// 読み出しシグネチャ（省略記法）
+type LogMessage = (message: string) => void
+export const logMessage6: LogMessage = (message) => {
+    console.log("function basic sample 6:",message) 
+}
+
+// 完全な呼び出しシグネチャ
+type FullLogMessage = {
+    (message: string):void
+}
+export const logMessage7: FullLogMessage = (message) => {
+    console.log("function basic sample 7:",message) 
+}
--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,23 @@
 // world.sayHello(root)
 
 // 03.基本の型定義
-import { primitiveSample, notExistSample, anySample, unknownSample } from './basic'
+// import { primitiveSample, notExistSample, anySample, unknownSample } from './basic'
 
-unknownSample()
-anySample()
-notExistSample()
-primitiveSample()
+// unknownSample()
+// anySample()
+// notExistSample()
+// primitiveSample()
+
+// 04.関数の型定義
+import {logMessage,logMessage2,logMessage3,logMessage4,alwaysThrowError} from "./function/basic"
+import {isUserSignedIn,isUserSignedIn2,sumProductsPrice} from "./function/parameters"
+// logMessage("Hello TypeScript!")
+// logMessage2("Hello TypeScript!")
+// logMessage3("Hello TypeScript!")
+// logMessage4("Hello TypeScript!")
+// alwaysThrowError("a")
+isUserSignedIn("ABC","Tora")
+isUserSignedIn("DEF")
+isUserSignedIn2("ABC")
+const sum = sumProductsPrice(10000000,1)
+console.log("Function parameters sample 5: ",sum)
