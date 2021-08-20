80bbfd065c6aa8f6389cfde54b99e0d6f00bcc99
zorro901 <zoro907@gmail.com>
2021-08-20 18:09:55 +0900

    lint-fix

--- src/world.ts
+++ src/world.ts
@@ -1,30 +1,11 @@
-// export default class World {
-//     message: string;
-//     constructor(message:string){
-//         this.message = message
-//     }
-//     public sayHello(elem: HTMLElement | null){
-//         if (elem){
-//             elem.innerText = this.message
-//         }
-//     }
-// }  
 export default class World {
-    // クラスで使うプロパティ
-    message: string
-  
-    // コンストラクタ（初期化）
-    constructor(message: string) {
-      this.message = message
-    }
-  
-    // 外部から呼び出せるメソッドを定義
-    public sayHello(elem: HTMLElement | null) {
-      if (elem) {
-        return (elem.innerText = this.message)
-      } else {
-        return
-      }
+  message: string
+  constructor(message: string) {
+    this.message = message
+  }
+  public sayHello(elem: HTMLElement | null): void {
+    if (elem) {
+      elem.innerText = this.message
     }
   }
-  
+}
