7b0b88e1026df726e2c5ed48e2b4d3bef756f95b
zorro901 <zoro907@gmail.com>
2021-08-21 13:48:53 +0900

    ジェネリック型を使いながらポリモーフィズムを理解する

--- src/generics/advanced.ts
+++ src/generics/advanced.ts
@@ -1,28 +1,28 @@
-import * as stream from "stream";
+import * as stream from 'stream'
 
-export default function genericsAdvancedSample(){
-    // map関数のシグネチャ
-    type Map<T, U> = (array: T[], fn: (item: T) => U) => U[]
+export default function genericsAdvancedSample() {
+  // map関数のシグネチャ
+  type Map<T, U> = (array: T[], fn: (item: T) => U) => U[]
 
-    const mapStringsToNumbers:Map<string, number> = (array, fn) => {
-        const result = []
-        for ( let i=0; i < array.length; i++){
-            const item = array[i]
-            result[i] = fn(item)
-        }
-        return result
+  const mapStringsToNumbers: Map<string, number> = (array, fn) => {
+    const result = []
+    for (let i = 0; i < array.length; i++) {
+      const item = array[i]
+      result[i] = fn(item)
     }
-    const numbers = mapStringsToNumbers(["123","456"],(item => Number(item)))
-    console.log("Generics advanced sample 1:",numbers)
+    return result
+  }
+  const numbers = mapStringsToNumbers(['123', '456'], (item) => Number(item))
+  console.log('Generics advanced sample 1:', numbers)
 
-    const mapNumbersToStrings:Map<number,string> = (array, fn) => {
-        const result = []
-        for ( let i=0; i < array.length; i++){
-            const item = array[i]
-            result[i] = fn(item)
-        }
-        return result
+  const mapNumbersToStrings: Map<number, string> = (array, fn) => {
+    const result = []
+    for (let i = 0; i < array.length; i++) {
+      const item = array[i]
+      result[i] = fn(item)
     }
-    const strings = mapNumbersToStrings(numbers,(item => String(item)))
-    console.log("Generics advanced sample 2:",strings)
+    return result
+  }
+  const strings = mapNumbersToStrings(numbers, (item) => String(item))
+  console.log('Generics advanced sample 2:', strings)
 }
